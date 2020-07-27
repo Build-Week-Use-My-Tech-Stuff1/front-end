5941ccd27374bbcef3d4b93ad92214d366e107a5
Devin Bidwell <dbidwell@intuituscyber.com>
2020-07-27 10:35:49 -0700

    form-state

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,30 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const initialLoginFormValues = {
-    username: "",
-    password: ""
-}
+  username: "",
+  password: "",
+};
 
 const initialRegisterFormValues = {
-    username: "",
-    password: "",
-    passwordConfirm: "",
-    firstName: "",
-    lastName: ""
-}
+  username: "",
+  password: "",
+  passwordConfirm: "",
+  firstName: "",
+  lastName: "",
+};
+
+const initialLoginErrorValues = {
+  username: "",
+  password: "",
+};
+
+const initialRegisterErrorValues = {
+  username: "",
+  password: "",
+  passwordConfirm: "",
+  firstName: "",
+  lastName: "",
+};
 
 const AuthContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
-export default function UserAuth(){
-    return(
-        <AuthContainer>
-            
-        </AuthContainer>
-    )
+export default function UserAuth() {
+  // Login form state
+  const [loginFormValues, setLoginFormValues] = useState(
+    initialLoginFormValues
+  );
+  // Register form state
+  const [registerFormValues, setRegisterFormValues] = useState(
+    initialRegisterFormValues
+  );
+  // Login form errors state
+  const [loginErrorValues, setLoginErrorValues] = useState(
+    initialLoginErrorValues
+  );
+  // Register form errors state
+  const [registerErrorValues, setRegisterErrorValues] = useState(
+    initialRegisterErrorValues
+  );
+
+  return <AuthContainer></AuthContainer>;
 }
