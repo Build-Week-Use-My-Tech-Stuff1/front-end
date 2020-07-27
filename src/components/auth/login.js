f4c01ea53a916950828894f0506912967667ce72
Devin Bidwell <dbidwell@intuituscyber.com>
2020-07-27 11:22:21 -0700

    form-placeholders

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,22 +1,20 @@
 import React from "react";
-import propTypes from 'prop-types';
+import propTypes from "prop-types";
 
 export default function Login(props) {
-    const {onUpdate, values, errors} = props;
+  const { onUpdate, values, errors } = props;
 
-    function update(event){
-        const {name, value} = event.target;
-        onUpdate(name, value);
-    }
+  function update(event) {
+    const { name, value } = event.target;
+    onUpdate(name, value);
+  }
 
-    function submit(event){
-        
-    }
+  function submit(event) {}
   return <form></form>;
 }
 
 Login.propTypes = {
-    onUpdate = propTypes.func.isRequired,
-    values = propTypes.object.isRequired,
-    errors = propTypes.object.isRequired
-}
+  onUpdate: propTypes.func.isRequired,
+  values: propTypes.object.isRequired,
+  errors: propTypes.object.isRequired,
+};
